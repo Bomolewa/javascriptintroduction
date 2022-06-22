23662a85fda0054917db1581d24fa4dc3ece4b8d
Bomolewa <105202044+Bomolewa@users.noreply.github.com>
2022-06-22 11:37:58 +0100

    first commit for zuri javascript registration assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+      // Declaring the strings 
+      let myname = "Bukola Kayode Olopade";
+      let myheight = "177cm";
+      let mycountry = "Nigeria";
+
+       //The javascript output    
+      document.write("My name is ");
+      document.write(myname);
+      document.write("<br>");
+      document.write("My Height is ");
+      document.write(myheight);
+      document.write("<br>");      
+      document.write("My Country is ");
+      document.write(mycountry);
+    
